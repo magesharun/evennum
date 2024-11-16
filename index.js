c792eeb49953003a0703da1b09155abfd415379b
magesh <magesharun2016@gmail.com>
2024-11-16 16:04:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function findEvenNumbers(arr) {
+    const evenNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        evenNumbers.push(arr[i]);
+      }
+    }
+    return evenNumbers;
+  }
+  
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const evenResult = findEvenNumbers(numbers);
+  console.log(evenResult); 
